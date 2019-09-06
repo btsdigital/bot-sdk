7bbdbd8a699a7a77dd2460e5766a4f7cee344b09
andrei.tkachev <andrei.tkachev@btsdigital.kz>
2019-09-06 17:22:50 +0600

    travis initial

---
+++ cd/before-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_c386e241d56c_key -iv $encrypted_c386e241d56c_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/signingkey.asc
+fi
